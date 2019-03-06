3e9e033074bf716ac072da026f7022a9cd120cf5
cpscott1 <thacam18@gmail.com>
2019-03-05 21:15:58 -0600

    added class to todo file

---
+++ src/components/TodoComponents/Todo.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Todo extends Component {
+  render() {
+    return (
+      <div>
+        <h2>TodoList</h2>
+      </div>
+    );
+  }
+}
+
+export default Todo;
